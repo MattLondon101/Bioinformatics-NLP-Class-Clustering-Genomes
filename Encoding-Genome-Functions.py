1abfeee89ea72922c9e8e425f70c4739a951754c
Matt London <matthewlondon101@gmail.com>
2020-03-17 01:12:02 -0400

    Update Encoding-Genome-Functions.py

--- Encoding-Genome-Functions.py
+++ Encoding-Genome-Functions.py
@@ -6,3 +6,38 @@ def string_to_array(my_string):
     my_array=np.array(list(my_string))
     return my_array
 
+# creates a label encoder with 'acgtn' alphabet
+from sklearn.preprocessing import LabelEncoder
+label_encoder=LabelEncoder()
+label_encoder.fit(np.array(['a','c','g','t','z']))
+
+# function to encode a DNA sequence string as an ordinal vector
+# returns a numpy vector with a=0.25, c=0.50, g=0.75, t=1.00, n=0.00
+def ordinal_encoder(my_array):
+    integer_encoded = label_encoder.transform(my_array)
+    float_encoded = integer_encoded.astype(float)
+    float_encoded[float_encoded == 0] = 0.25 # A
+    float_encoded[float_encoded == 1] = 0.50 # C
+    float_encoded[float_encoded == 2] = 0.75 # G
+    float_encoded[float_encoded == 3] = 1.00 # T
+    float_encoded[float_encoded == 4] = 0.00 # anything else, z
+    return float_encoded
+
+# function to one-hot encode a DNA sequence string
+# non 'acgt' bases (n) are 0000
+# returns a L x 4 numpy array
+from sklearn.preprocessing import OneHotEncoder
+def one_hot_encoder(my_array):
+    integer_encoded = label_encoder.transform(my_array)
+    onehot_encoder = OneHotEncoder(sparse=False, dtype=int, n_values=5)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    onehot_encoded = np.delete(onehot_encoded, -1, 1)
+    return onehot_encoded
+
+#function that can be used to convert any sequence (string) to overlapping k-mer words:
+def getKmers(sequence, size):
+    return [sequence[x:x+size].lower() for x in range(len(sequence) - size + 1)]   
+
+
+
