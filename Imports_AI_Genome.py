1030de47081f7322fb89da074ebd727e5d687748
Matt London <matthewlondon101@gmail.com>
2020-03-17 02:11:13 -0400

    Update Imports_AI_Genome.py

--- Imports_AI_Genome.py
+++ Imports_AI_Genome.py
@@ -1,6 +1,8 @@
 # Bio = biopython
 from Bio import SeqIO
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import re
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
