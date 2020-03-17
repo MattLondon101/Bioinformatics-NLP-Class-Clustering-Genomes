b8e0792f5a505f3ef57a83baa2fd2c74dffca366
Matt London <matthewlondon101@gmail.com>
2020-03-17 03:26:58 -0400

    Update Imports_AI_Genome.py

--- Imports_AI_Genome.py
+++ Imports_AI_Genome.py
@@ -1,9 +1,11 @@
 # Bio = biopython
 from Bio import SeqIO
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import seaborn as sns
 import re
 from sklearn.preprocessing import LabelEncoder
