c8c84683ca58cedfc9f61c402a23e8a559df72c0
Matt London <matthewlondon101@gmail.com>
2020-05-04 15:47:38 -0400

    Update Imports_AI_Genome.py

--- Imports_AI_Genome.py
+++ Imports_AI_Genome.py
@@ -12,3 +12,7 @@ import seaborn as sns
 import re
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 200)
